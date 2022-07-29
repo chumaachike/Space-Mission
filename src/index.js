99c6be908b8a0c88e3cbe8ff41bfb13b919b0a0d
Achike Chuma Edward <50702933+chumaachike@users.noreply.github.com>
2022-07-29 04:03:52 -0700

    rename config store

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './components/App';
-import ConfigureStore from './redux/ConfigureStore.js';
+import ConfigureStore from './redux/ConfigureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
